d95e204bed1ded606da4bd64cc8af513d5de8943
David Ho <davidhdoev@gmail.com>
2020-02-24 16:12:40 -0500

    Background placed and newBillViewController started

--- BillSplitter/newBillViewController.swift
+++ BillSplitter/newBillViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 import TesseractOCR
 
 class newBillViewController: UIViewController, G8TesseractDelegate {
+    @IBOutlet weak var label: UILabel!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: "newBillViewController", bundle: nil)
+    }
+       
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        let label = UILabel(frame: view.frame);
-        label.text = "test";
-        label.backgroundColor = UIColor.white;
-        view.addSubview(label);
+        self.view.backgroundColor = UIColor(red:0.95, green:0.92, blue:0.98, alpha:1.0)
         
         if let tesseract = G8Tesseract(language: "eng") {
             tesseract.delegate = self
             tesseract.image = UIImage(named: "demoReceipt")
+            tesseract.setVariableValue("0123456789", forKey: "tessedit_char_whitelist")
+//            tesseract.pageSegmentationMode = .auto
             tesseract.recognize()
             label.text = tesseract.recognizedText
         }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
     func progressImageRecognition(for tesseract: G8Tesseract!) {
